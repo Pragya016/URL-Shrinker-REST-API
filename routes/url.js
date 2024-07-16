40c2db381901dd90247d04893e95d0524091ff15
Pragya Saxena <pragyasaxena8279@gmail.com>
2024-07-16 13:57:59 +0530

    added new features

--- routes/url.js
+++ routes/url.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import shortid from 'shortid'
 import URL from '../models/URL.js';
+import moment from 'moment';
 
 const router = express.Router();
+
+// get all urls
+router.get('/', async (req, res) => {
+    try {
+        const urls = await URL.find();
+        res.status(200).json(urls);
+    } catch (error) {
+        res.status(500).send('Internal server error.');
+    }
+})
 
 // Shorten URL
 router.post('/shorten', async (req, res) => {
@@ -10,8 +21,9 @@ router.post('/shorten', async (req, res) => {
     const shortUrl = customCode || shortid.generate();
     const url = new URL({ fullUrl, shortUrl });
     await url.save();
-    res.json({ shortUrl });
+    res.status(201).json({ shortUrl });
 });
+
 
 // Redirect and Track
 router.get('/:shortUrl', async (req, res) => {
@@ -20,6 +32,12 @@ router.get('/:shortUrl', async (req, res) => {
 
         if (!shortUrl) {
             return res.status(404).send('Short URL not found');
+        }
+
+        const currentTime = moment();
+        const expireAtTime = moment(shortUrl.expireAt, 'hh:mm A');
+        if (currentTime.isAfter(expireAtTime)) {
+            return res.status(410).send('URL has expired');
         }
 
         shortUrl.visits++;
@@ -38,7 +56,13 @@ router.get('/analytics/:shortUrl', async (req, res) => {
         const shortUrl = await URL.findOne({ shortUrl: req.params.shortUrl });
         if (!shortUrl) return res.status(404).send('Short URL not found');
 
-        res.json({
+        const currentTime = moment();
+        const expireAtTime = moment(shortUrl.expireAt, 'hh:mm A');
+        if (currentTime.isAfter(expireAtTime)) {
+            return res.status(410).send('URL has expired');
+        }
+
+        res.status(200).json({
             originalUrl: shortUrl.fullUrl,
             shortUrl: shortUrl.shortUrl,
             visits: shortUrl.visits,
